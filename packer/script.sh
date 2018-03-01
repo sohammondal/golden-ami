3a819d2ce61e7219b36eaca691907e4ca6246ccc
sohammondal <34343502+sohammondal@users.noreply.github.com>
2018-03-01 10:41:22 +0530

    Update script.sh

--- packer/script.sh
+++ packer/script.sh
@@ -4,19 +4,22 @@ echo "*******************************************************************"
 echo "*************************WELCOME TO PACKER*************************"
 echo "*******************************************************************"
 
-sudo su
+export PATH=/usr/local/bin:/usr/bin:/root/.local/bin:$PATH
 
 
 #WGET & UNZIP
 echo "*************************Installing wget and unzip*************************"
-yum -y install wget unzip
+yum -y install wget unzip python34
 
-# PIP,ANSIBLE & DEPENDENCIES
-echo "*************************Installing pip*************************"
-easy_install pip
 
 echo "*************************Installing dependency for ansible*************************"
 yum install gcc libffi-devel python-devel openssl-devel -y #dependency for ansible #need python-devel
+
+
+# PIP,ANSIBLE & DEPENDENCIES
+echo "*************************Installing pip*************************"
+curl -O https://bootstrap.pypa.io/get-pip.py
+python3 get-pip.py --user
 
 echo "*************************Installing ansible*************************"
 pip install ansible==2.4.3.0
@@ -24,7 +27,7 @@ pip install ansible==2.4.3.0
 
 #AWS Inspector
 echo "*************************Installing AWS Inspector*************************"
-wget https://d1wk0tztpsntt1.cloudfront.net/linux/latest/install
+sudo wget https://d1wk0tztpsntt1.cloudfront.net/linux/latest/install
 bash install
 rm -rf install
 
@@ -49,7 +52,6 @@ yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/li
 systemctl start amazon-ssm-agent
 systemctl enable amazon-ssm-agent
 
-export PATH=/usr/local/bin:/usr/bin:/root/.local/bin:$PATH
 export AWS_ACCESS_KEY_ID={{user `aws_access_key`}}
 export AWS_SECRET_ACCESS_KEY={{user `aws_secret_key`}}
 sudo tar -cvf /root/etc_bak.tar /etc
