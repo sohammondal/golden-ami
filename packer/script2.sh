62197e6fb53ff7093b0055f3ffd1aad7e11042d5
sohammondal <34343502+sohammondal@users.noreply.github.com>
2018-03-06 18:22:45 +0530

    Update script2.sh

--- packer/script2.sh
+++ packer/script2.sh
@@ -7,7 +7,7 @@ sudo wget https://d1wk0tztpsntt1.cloudfront.net/linux/latest/install
 sudo bash install
 rm -rf install
 sudo pip install awscli --upgrade --user
-sudo yum install perl-Switch perl-DateTime perl-Sys-Syslog perl-LWP-Protocol-https -y
+sudo yum install perl-Switch perl-DateTime perl-Sys-Syslog perl-LWP-Protocol-https perl-Digest-SHA.x86_64 -y
 mkdir aws-cloudwatch
 cd aws-cloudwatch
 sudo curl http://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScripts-1.2.1.zip -O
